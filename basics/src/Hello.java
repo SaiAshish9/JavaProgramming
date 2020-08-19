ca10af6b2299e2f6169d9da7e2cb8ca69c5d5c30
SaiAshish9 <saiashish3760@gmail.com>
2020-08-19 15:49:22 +0530

    k

--- basics/src/Hello.java
+++ basics/src/Hello.java
@@ -2,5 +2,8 @@ public class Hello {
     public static void main(String[] args)
     {
         System.out.println("hello world");
+        int a= 5;
+        int b=8;
+        System.out.println(a+b);
     }
 }
---
+++ datatypes/src/com/javaprogramming/Main.java
@@ -0,0 +1,52 @@
+package com.javaprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        long a=100;
+        long b=100L; 
+        int c=2_147_483_647;
+        System.out.print(c);
+        System.out.println(a);
+        System.out.println(b);
+	// write your code here
+        System.out.println("Byte");
+        System.out.println(Byte.MIN_VALUE);
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Byte.SIZE);
+        System.out.println("Short");
+        System.out.println(Short.MIN_VALUE);
+        System.out.println(Short.MAX_VALUE);
+        System.out.println(Short.SIZE);
+        System.out.println("Int");
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.SIZE);
+        System.out.println("Float");
+        System.out.println(Float.MIN_VALUE);
+        System.out.println(Float.MAX_VALUE);
+        System.out.println(Float.SIZE);
+        System.out.println("Double");
+        System.out.println(Double.MIN_VALUE);
+        System.out.println(Double.MAX_VALUE);
+        System.out.println(Double.SIZE);
+        System.out.println("Long");
+        System.out.println(Long.MIN_VALUE);
+        System.out.println(Long.MAX_VALUE);
+        System.out.println(Long.SIZE);
+        System.out.println(2_147_483_647_234L);
+        float f=5.25f;
+        float f1=(float)5.25;
+//        BigDecimal Java Class
+//        f is important
+        char c1='\u00A9';
+        boolean b1=true;
+        String m="\u00A9 ghcghcgh";
+        System.out.println(m);
+        System.out.println(!b1);
+        System.out.println(c1);
+        System.out.println(f);
+
+    }
+}
