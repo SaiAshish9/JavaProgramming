4bafc84edff98c21d41c9ab0f459424e1d3d4f0f
SaiAshish9 <saiashish3760@gmail.com>
2020-09-15 15:30:33 +0530

    sqlite3 updated

---
+++ database/src/com/javaprogramming/Main.java
@@ -0,0 +1,16 @@
+package com.javaprogramming;
+
+//  database stores data in a single file
+//  flat file database stores all data in a single table
+//  this results in data duplication
+
+// view is a selection of rows and columns
+// possible from more than one table.
+// sqlite3
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
